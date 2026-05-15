2d0d0b673e7f765c1f9fe63d2ce5bdea03246a22
Andrey <andreyenrique132@gmail.com>
2026-05-15 14:32:20 -0700

    The "Edit level" option was added so that if the user is an administrator they can configure the levels

--- entities/word.py
+++ entities/word.py
@@ -8,6 +8,7 @@ class Word:
         self.word = word
         self.phrase = phrase
 
+
     @classmethod
     def wordbyId(cls,id):
         connection = get_connection()
@@ -26,3 +27,31 @@ class Word:
             return cls(row['id'], row['word'], row['phrase'])
         else:
             return None
+
+
+    @classmethod
+    def list_ids(cls):
+        connection = get_connection()
+        cursor = connection.cursor(pymysql.cursors.DictCursor)
+        sql = "SELECT id FROM words ORDER BY id"
+        cursor.execute(sql)
+        rows = cursor.fetchall()
+        cursor.close()
+        connection.close()
+        return [r['id'] for r in rows]
+
+
+    @classmethod
+    def update_word(cls, id, word, phrase):
+        try:
+            connection = get_connection()
+            cursor = connection.cursor()
+            sql = "UPDATE words SET word = %s, phrase = %s WHERE id = %s"
+            cursor.execute(sql, (word, phrase, id))
+            connection.commit()
+            cursor.close()
+            connection.close()
+            return True
+        except Exception as ex:
+            print(f"Error updating word: {ex}")
+            return False
